9e6953aac074a7c14ebdc645f1dc70306a5de9a8
torquescrew <sugto555@gmail.com>
2014-01-15 15:35:24 +1300

    Tidy up

--- public/scripts/dialog.js
+++ public/scripts/dialog.js
@@ -1,180 +1,10 @@
+/*global u: flase, pane: false */
 /**
- * Created by tobysuggate on 11/01/14.
+ * Created by tobysuggate on 15/01/14.
  */
+
 
 "use strict";
-
-var dlg = dlg || {};
-
-
-dlg.handle = null;
-dlg.closer = null;
-dlg.window = null;
-dlg.content = null;
-dlg.grabberSE = null;
-
-dlg.body = $('body');
-dlg.mousePos = new Pos(0, 0);
-dlg.beginPos = new Pos(0, 0);
-
-
-/** @returns {void} */
-dlg.setup = function () {
-
-  dlg.setupHandle();
-  dlg.setupGrabbers();
-  dlg.setupCloser();
-};
-
-
-dlg.open = function (title, content) {
-  var id = u.createId();
-
-  dlg.body.append(
-      '<div class="dialog" id="dialog' + id + '">'
-      + '<div class="deco" id="deco' + id + '">'
-        + '<div class="handle" id="handle' + id + '"><h3 class="decTitle">' + title + '</h3></div>'
-        + '<div class="closer" id="closer' + id + '"></div>'
-      + '</div>'
-      + '<div class="content">'
-        + content
-        + '<div id="grabberSE"></div>'
-      + '</div>'
-    + '</div>'
-  );
-
-  dlg.setupAccessors(id);
-
-  dlg.setup();
-  dlg.centreDialog();
-};
-
-
-/**
- * @param {string} id
- */
-dlg.setupAccessors = function (id) {
-  dlg.handle = $('#handle' + id);
-  dlg.closer = $('#closer' + id);
-  dlg.window = $('#dialog' + id);
-  dlg.content = $('.content');
-  dlg.grabberSE = $('#grabberSE');
-};
-
-
-dlg.setupCloser = function () {
-
-  dlg.drawCloser();
-
-  dlg.closer.hover(function () {
-    dlg.closer.css({ 'background-color': '#444444' });
-  }, function () {
-    dlg.closer.css('background-color', 'inherit');
-  });
-
-  dlg.closer.click(function () {
-    dlg.close();
-  });
-};
-
-
-/** @returns {void} */
-dlg.close = function () {
-  dlg.window.remove();
-};
-
-
-/** @returns {void} */
-dlg.centreDialog = function () {
-  var pageWidth = $(window).width();
-  var pageHeight = $(window).height();
-
-  var modW = dlg.window.width() / 2;
-  var modH = dlg.window.height() / 2;
-
-  var x = (pageWidth / 2) - modW;
-  var y = ((pageHeight / 2) - modH) / 2;
-
-  dlg.window.css({ left: x, top: y });
-};
-
-
-/** @returns {void} */
-dlg.setupHandle = function () {
-  dlg.handle.on('mousedown', function (evt) {
-    evt.preventDefault(); // prevent text cursor
-
-    var off = dlg.window.offset();
-    dlg.savePositions(off.left, off.top, evt);
-
-    pane.createAndGet().on('mousemove', function (e) {
-      var change = dlg.getChange(e);
-
-      var x = dlg.beginPos.x + change.x;
-      var y = dlg.beginPos.y + change.y;
-
-      dlg.window.css({ left: x, top: y });
-    });
-  });
-};
-
-
-/**
- * @param {number} x
- * @param {number} y
- * @param {Event} evt
- */
-dlg.savePositions = function (x, y, evt) {
-  dlg.mousePos = new Pos(evt.clientX, evt.clientY);
-  dlg.beginPos = new Pos(x, y);
-};
-
-
-/**
- * @param {Event} evt
- * @returns {Pos}
- */
-dlg.getChange = function (evt) {
-  var x = (evt.clientX - dlg.mousePos.x);
-  var y = (evt.clientY - dlg.mousePos.y);
-
-  return new Pos(x, y);
-};
-
-
-/** @returns {void} */
-dlg.setupGrabbers = function () {
-  dlg.grabberSE.on('mousedown', function (e) {
-    e.preventDefault(); // prevent text cursor
-
-    dlg.savePositions(dlg.content.width(), dlg.content.height(), e);
-
-    pane.createAndGet().on('mousemove', function (e) {
-      var change = dlg.getChange(e);
-
-      dlg.window.css({ width: '' + (dlg.beginPos.x + change.x) + 'px' });
-      dlg.content.css({ width: '' + (dlg.beginPos.x + change.x) + 'px', height: '' + (dlg.beginPos.y + change.y) + 'px'});
-    });
-  });
-};
-
-
-dlg.drawCloser = function () {
-  dlg.closer.append('<canvas id="closerCanvas" width="100" height="100"></canvas>');
-
-  var context = document.getElementById("closerCanvas").getContext("2d");
-  context.lineWidth = 10;
-  context.strokeStyle = '#ffffff';
-
-  context.moveTo(30,30);
-  context.lineTo(70,70);
-  context.stroke();
-
-  context.moveTo(70,30);
-  context.lineTo(30,70);
-  context.stroke();
-};
-
 
 /**
  * @param {number} x
@@ -186,5 +16,159 @@ function Pos(x, y) {
   this.y = y;
 }
 
-//dlg.open('hello');
 
+/**
+ * @param {string} title
+ * @param {string} content
+ * @constructor
+ */
+function Dialog(title, content) {
+
+  var self = this;
+  var mTitle = title;
+  var mHTMLContent = content;
+  var mId = u.createId();
+
+  var mHandle = null;
+  var mCloser = null;
+  var mWindow = null;
+  var mContent = null;
+  var mGrabberSE = null;
+
+  var mBody = $('body');
+  var mMousePos = new Pos(0, 0);
+  var mBeginPos = new Pos(0, 0);
+
+  self.open = function () {
+    mBody.append(
+      '<div class="dialog" id="dialog' + mId + '">' +
+        '<div class="deco" id="deco' + mId + '">' +
+          '<div class="handle" id="handle' + mId + '"><h3 class="decTitle">' + mTitle + '</h3></div>' +
+          '<div class="closer" id="closer' + mId + '"></div>' +
+        '</div>' +
+        '<div class="content">' +
+          mHTMLContent +
+          '<div id="grabberSE"></div>' +
+        '</div>' +
+      '</div>'
+    );
+
+    self.setupAccessors();
+    self.setupHandle();
+    self.setupGrabbers();
+    self.setupCloser();
+    self.centreDialog();
+  };
+
+  self.close = function () {
+    mWindow.remove();
+  };
+
+  self.setupAccessors = function () {
+    mHandle = $('#handle' + mId);
+    mCloser = $('#closer' + mId);
+    mWindow = $('#dialog' + mId);
+    mContent = $('.content');
+    mGrabberSE = $('#grabberSE');
+  };
+
+  self.centreDialog = function () {
+    var pageWidth = $(window).width();
+    var pageHeight = $(window).height();
+
+    var modW = mWindow.width() / 2;
+    var modH = mWindow.height() / 2;
+
+    var x = (pageWidth / 2) - modW;
+    var y = ((pageHeight / 2) - modH) / 2;
+
+    mWindow.css({ left: x, top: y });
+  };
+
+  self.setupHandle = function () {
+    mHandle.on('mousedown', function (evt) {
+      evt.preventDefault(); // prevent text cursor
+
+      var off = mWindow.offset();
+      self.savePositions(off.left, off.top, evt);
+
+      pane.createAndGet().on('mousemove', function (e) {
+        var change = self.getChange(e);
+
+        var x = mBeginPos.x + change.x;
+        var y = mBeginPos.y + change.y;
+
+        mWindow.css({ left: x, top: y });
+      });
+    });
+  };
+
+  /**
+   * @param {number} x
+   * @param {number} y
+   * @param {Event} evt
+   */
+  self.savePositions = function (x, y, evt) {
+    mMousePos = new Pos(evt.clientX, evt.clientY);
+    mBeginPos = new Pos(x, y);
+  };
+
+  /**
+   * @param {Event} evt
+   * @returns {Pos}
+   */
+  self.getChange = function (evt) {
+    var x = (evt.clientX - mMousePos.x);
+    var y = (evt.clientY - mMousePos.y);
+
+    return new Pos(x, y);
+  };
+
+  /** @returns {void} */
+  self.drawCloser = function () {
+    mCloser.append('<canvas id="closerCanvas" width="100" height="100"></canvas>');
+
+    var context = document.getElementById("closerCanvas").getContext("2d");
+    context.lineWidth = 10;
+    context.strokeStyle = '#ffffff';
+
+    context.moveTo(30,30);
+    context.lineTo(70,70);
+    context.stroke();
+
+    context.moveTo(70,30);
+    context.lineTo(30,70);
+    context.stroke();
+  };
+
+  /** @returns {void} */
+  self.setupCloser = function () {
+    self.drawCloser();
+
+    mCloser.hover(function () {
+      mCloser.css({ 'background-color': '#444444' });
+    }, function () {
+      mCloser.css('background-color', 'inherit');
+    });
+
+    mCloser.click(function () {
+      self.close();
+    });
+  };
+
+  /** @returns {void} */
+  self.setupGrabbers = function () {
+    mGrabberSE.on('mousedown', function (e) {
+      e.preventDefault(); // prevent text cursor
+
+      self.savePositions(mContent.width(), mContent.height(), e);
+
+      pane.createAndGet().on('mousemove', function (e) {
+        var change = self.getChange(e);
+
+        mWindow.css({ width: '' + (mBeginPos.x + change.x) + 'px' });
+        mContent.css({ width: '' + (mBeginPos.x + change.x) + 'px', height: '' + (mBeginPos.y + change.y) + 'px'});
+      });
+    });
+  };
+}
